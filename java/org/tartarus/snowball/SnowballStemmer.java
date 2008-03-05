0bbb91aaba873e3d7c198416db804d07a15e7b43
Richard Boulton <richard@tartarus.org>
2008-03-05 22:59:40 +0000

    Add Oerd Cukalla to AUTHORS, and remove pointless duplicated code from the SnowballStemmer constructor.
    git-svn-id: svn+userv://snowball.tartarus.org/snowball/trunk/snowball@503 633ccae0-01f4-0310-8c99-d3591da6f01f

--- java/org/tartarus/snowball/SnowballStemmer.java
+++ java/org/tartarus/snowball/SnowballStemmer.java
@@ -3,11 +3,5 @@ package org.tartarus.snowball;
 import java.lang.reflect.InvocationTargetException;
 
 public abstract class SnowballStemmer extends SnowballProgram {
-    protected SnowballStemmer()
-    {
-	current = new StringBuffer();
-	setCurrent("");
-    }
-
     public abstract boolean stem();
 };
