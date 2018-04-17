051c03c0924cd7e7b5d329218e69306d6e5e068a
Olly Betts <olly@survex.com>
2018-04-17 14:55:12 +1200

    [rust] Fix generate_dollar()

--- compiler/generator_rust.c
+++ compiler/generator_rust.c
@@ -853,6 +853,7 @@ static void generate_dollar(struct generator * g, struct node * p) {
     generate(g, p->left);
     if (!g->unreachable) {
         g->V[0] = p->name;
+        g->B[0] = str_data(savevar_env);
         /* Update string variable. */
         w(g, "~M~V0 = env.current.clone().into_owned();~N");
         /* Reset env */
