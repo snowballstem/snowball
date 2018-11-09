db198aace16bc59ec4f9a0116a2f084bca1fbeef
Olly Betts <olly@survex.com>
2018-11-10 09:51:34 +1300

    [rust] Optimise setlimit tomark AE for ...
    All uses of setlimit in the current stemmers we ship follow this
    pattern, and by special-casing we can avoid having to save and restore
    the cursor.
    
    Addresses https://github.com/snowballstem/snowball/issues/74 for Rust.

--- compiler/generator_rust.c
+++ compiler/generator_rust.c
@@ -781,19 +781,32 @@ static void generate_setlimit(struct generator * g, struct node * p) {
     struct str * savevar = vars_newname(g);
     struct str * varname = vars_newname(g);
     write_comment(g, p);
-    write_savecursor(g, p, savevar);
-    generate(g, p->left);
+    if (p->left && p->left->type == c_tomark && !p->left->right) {
+        /* Special case for:
+         *
+         *   setlimit tomark AE for C
+         *
+         * All uses of setlimit in the current stemmers we ship follow this
+         * pattern, and by special-casing we can avoid having to save and
+         * restore c.
+         */
+        struct node * q = p->left;
+        g->S[0] = q->mode == m_forward ? ">" : "<";
+        w(g, "~Mif env.cursor ~S0 "); generate_AE(g, q->AE); w(g, " ");
+        write_block_start(g);
+        write_failure(g);
+        write_block_end(g);
+        g->unreachable = false;
 
-    if (!g->unreachable) {
         g->B[0] = str_data(varname);
         if (p->mode == m_forward) {
             w(g, "~Mlet ~B0 = env.limit - env.cursor;~N");
-            w(g, "~Menv.limit = env.cursor;~N");
+            w(g, "~Menv.limit = ");
         } else {
             w(g, "~Mlet ~B0 = env.limit_backward;~N");
-            w(g, "~Menv.limit_backward = env.cursor;~N");
+            w(g, "~Menv.limit_backward = ");
         }
-        write_restorecursor(g, p, savevar);
+        generate_AE(g, q->AE); writef(g, ";~N", q);
 
         if (p->mode == m_forward) {
             str_assign(g->failure_str, "env.limit += ");
@@ -804,7 +817,34 @@ static void generate_setlimit(struct generator * g, struct node * p) {
             str_append(g->failure_str, varname);
             str_append_string(g->failure_str, ";");
         }
+    } else {
+        write_savecursor(g, p, savevar);
+        generate(g, p->left);
 
+        if (!g->unreachable) {
+            g->B[0] = str_data(varname);
+            if (p->mode == m_forward) {
+                w(g, "~Mlet ~B0 = env.limit - env.cursor;~N");
+                w(g, "~Menv.limit = env.cursor;~N");
+            } else {
+                w(g, "~Mlet ~B0 = env.limit_backward;~N");
+                w(g, "~Menv.limit_backward = env.cursor;~N");
+            }
+            write_restorecursor(g, p, savevar);
+
+            if (p->mode == m_forward) {
+                str_assign(g->failure_str, "env.limit += ");
+                str_append(g->failure_str, varname);
+                str_append_string(g->failure_str, ";");
+            } else {
+                str_assign(g->failure_str, "env.limit_backward = ");
+                str_append(g->failure_str, varname);
+                str_append_string(g->failure_str, ";");
+            }
+        }
+    }
+
+    if (!g->unreachable) {
         generate(g, p->aux);
 
         if (!g->unreachable) {
