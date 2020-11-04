23534a738373f0c77802e5246f343728dcc16549
Olly Betts <olly@survex.com>
2020-11-04 13:20:42 +1300

    Fix stemtest to compile in C90 mode

--- tests/stemtest.c
+++ tests/stemtest.c
@@ -26,15 +26,19 @@ static const struct testcase {
 int
 main(int argc, char * argv[])
 {
+    const struct testcase * p;
     (void)argc;
     (void)argv;
-    for (const struct testcase * p = testcases; p->language; ++p) {
+    for (p = testcases; p->language; ++p) {
 	const char * language = p->language;
 	const char * charenc = p->charenc;
 	const char * input = p->input;
 	const char * expect = p->expect;
-	if (expect == NULL) expect = input;
 	struct sb_stemmer * stemmer = sb_stemmer_new(language, charenc);
+	const sb_symbol * stemmed;
+	int len;
+
+	if (expect == NULL) expect = input;
 	if (stemmer == 0) {
 	    if (charenc == NULL) {
 		fprintf(stderr, "language `%s' not available for stemming\n", language);
@@ -44,13 +48,13 @@ main(int argc, char * argv[])
 		exit(1);
 	    }
 	}
-	const sb_symbol * stemmed = sb_stemmer_stem(stemmer, (const unsigned char*)input, strlen(input));
+	stemmed = sb_stemmer_stem(stemmer, (const unsigned char*)input, strlen(input));
 	if (stemmed == NULL) {
 	    fprintf(stderr, "Out of memory");
 	    exit(1);
 	}
 
-	int len = sb_stemmer_length(stemmer);
+	len = sb_stemmer_length(stemmer);
 	if (len != (int)strlen(expect) || memcmp(stemmed, expect, len) != 0) {
 	    fprintf(stderr, "%s stemmer output for %s was %.*s not %s\n",
 			    language, input, len, stemmed, expect);
