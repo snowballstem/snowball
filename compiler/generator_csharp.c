d6a3e7d1765c8c655d1ff6cc33c14fbf41e9d7b2
Turnerj <git@turnerj.com>
2021-04-08 15:50:28 +0930

    Define among values as static

--- compiler/generator_csharp.c
+++ compiler/generator_csharp.c
@@ -1188,19 +1188,10 @@ static void generate_class_end(struct generator * g) {
 
 static void generate_among_declaration(struct generator * g, struct among * x) {
 
-    g->I[0] = x->number;
-    g->I[1] = x->literalstring_count;
-
-    w(g, "~Mprivate readonly Among[] a_~I0;~N");
-}
-
-static void generate_among_table(struct generator * g, struct among * x) {
-
     struct amongvec * v = x->b;
-
     g->I[0] = x->number;
 
-    w(g, "~Ma_~I0 = new[] ~N~M{~N~+");
+    w(g, "~Mprivate static readonly Among[] a_~I0 = new[] ~N~M{~N~+");
     {
         int i;
         for (i = 0; i < x->literalstring_count; i++) {
@@ -1224,26 +1215,11 @@ static void generate_among_table(struct generator * g, struct among * x) {
 static void generate_amongs(struct generator * g) {
     struct among * x;
 
-    /* Generate declarations. */
     x = g->analyser->amongs;
     while (x != 0) {
         generate_among_declaration(g, x);
         x = x->next;
     }
-
-    w(g, "~N");
-    w(g, "~M/// <summary>~N");
-    w(g, "~M///   Initializes a new instance of the <see cref=\"~n\"/> class.~N");
-    w(g, "~M/// </summary>~N");
-    w(g, "~M/// ~N");
-    w(g, "~Mpublic ~n()~N~{");
-    x = g->analyser->amongs;
-    while (x != 0) {
-        generate_among_table(g, x);
-        x = x->next;
-    }
-
-    w(g, "~}~N~N");
 }
 
 static void generate_grouping_table(struct generator * g, struct grouping * q) {
