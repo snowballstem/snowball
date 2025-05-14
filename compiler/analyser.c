snowballstem/snowball

commit 9dcd62b802311d2d2d9fcf8667f7df79c1e01476

Improve placement of functionend nodes
For a function with a command list as the body (i.e. all non-trivial
functions) the functionend node is now placed at the end of that
top-level command list instead of after it, which means tail-calling and
similar optimisations are now triggered in more cases.

compiler/analyser.c
@@ -1530,7 +1530,15 @@ static void read_define_routine(struct analyser * a, struct name * q) {
      * can easily see where the function ends.
      */
     assert(p->left->right == NULL);
-    p->left->right = new_node(a, c_functionend);
+    if (p->left->type == c_bra) {
+        /* Put the "functionend" node at the end of the command list. */
+        struct node * e = p->left->left;
+        while (e->right) e = e->right;
+        e->right = new_node(a, c_functionend);
+    } else {
+        /* Put the "functionend" node after the single command. */
+        p->left->right = new_node(a, c_functionend);
+    }
 
     if (a->substring != NULL) {
         error2(a, e_unresolved_substring, a->substring->line_number);

compiler/generator_ada.c
@@ -1059,7 +1059,8 @@ static void generate_call(struct generator * g, struct node * p) {
     if (g->failure_label == x_return) {
         if (p->right && p->right->type == c_functionend) {
             /* Tail call. */
-            writef(g, "~M~V0 (Z, Result);~N", p);
+            writef(g, "~M~V0 (Z, Result);~N~Mreturn;~N", p);
+            p->right = NULL;
             return;
         }
         if (signals == 0) {

compiler/generator_pascal.c
@@ -1064,6 +1064,7 @@ static void generate_call(struct generator * g, struct node * p) {
         if (p->right && p->right->type == c_functionend) {
             /* Tail call. */
             writef(g, "~MResult := ~V0;~N", p);
+            p->right = NULL;
             return;
         }
         if (signals == 0) {

commit 9507d8a00efb4eb5251ec32f6cd29f96271ec6c5

Fix segfault for routine with () as body
Bug introduced by 9dcd62b802311d2d2d9fcf8667f7df79c1e01476 so not
present in any release.

compiler/analyser.c
@@ -1545,8 +1545,12 @@ static void read_define_routine(struct analyser * a, struct name * q) {
     if (p->left->type == c_bra) {
         /* Put the "functionend" node at the end of the command list. */
         struct node * e = p->left->left;
-        while (e->right) e = e->right;
-        e->right = new_node(a, c_functionend);
+        if (e) {
+            while (e->right) e = e->right;
+            e->right = new_node(a, c_functionend);
+        } else {
+            p->left = new_node(a, c_functionend);
+        }
     } else {
         /* Put the "functionend" node after the single command. */
         p->left->right = new_node(a, c_functionend);