snowballstem/snowball

commit 5ef33aef14f8dc39a3290343024f10157fd15a36

Handle new synthesised commands
Take them into account in check_possible_signals() and repeat_score().
Fixes minor codegen efficiency regressions.

compiler/generator.c
@@ -536,6 +536,10 @@ static int check_possible_signals(struct generator * g,
                                                call_depth + 1);
         case c_gopast:
         case c_goto:
+        case c_goto_grouping:
+        case c_gopast_grouping:
+        case c_goto_non:
+        case c_gopast_non:
             /* FIXME: unless we can prove that c is either definitely atlimit
              * or definitely not atlimit... */
             return -1;
@@ -771,6 +775,10 @@ static int repeat_score(struct generator * g, struct node * p, int call_depth) {
             case c_next:
             case c_grouping:
             case c_non:
+            case c_goto_grouping:
+            case c_gopast_grouping:
+            case c_goto_non:
+            case c_gopast_non:
             case c_hop:
                 if (++score >= 2)
                     return score;

commit 3021622e496a0211f1feb11f568abb4bd0da7155

Turn off flawed optimisation for now
Don't optimise repeat/atleast applied to goto/gopast on a (non)
grouping.  This optimisation requires that the code in the loop
preserves the cursor's value on failure, and the target language helper
functions used here doesn't currently (they probably easily could
but let's handle that separately).

Looking at the stemmers we ship, this affects the code generated for one
loop in indonesian.sbl, but it happens the cursor value is overwritten
immediately after this loop anyway.  The bug could affect non-shipped
Snowball code though so isn't purely latent.

Bug introduced by 5ef33aef14f8dc39a3290343024f10157fd15a36 which
was in v3.0.0.

compiler/generator.c
@@ -537,10 +537,14 @@ static int repeat_score(struct generator * g, struct node * p, int call_depth) {
             case c_next:
             case c_grouping:
             case c_non:
+#if 0
+            // These could be here if the target-language helpers all preserved
+            // the cursor on failure:
             case c_goto_grouping:
             case c_gopast_grouping:
             case c_goto_non:
             case c_gopast_non:
+#endif
             case c_hop:
                 if (++score >= 2)
                     return score;