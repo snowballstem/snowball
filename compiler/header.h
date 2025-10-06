a66d4180022ef85c1f2c4b11b5a0d24ec02acf10
Olly Betts <olly@survex.com>
2025-10-07 10:45:27 +1300

    Alphabetise target_lang enum values

--- compiler/header.h
+++ compiler/header.h
@@ -398,7 +398,20 @@ struct options {
     byte syntax_tree;
     byte comments;
     enc encoding;
-    enum { LANG_JAVA, LANG_C, LANG_CPLUSPLUS, LANG_CSHARP, LANG_DART, LANG_PASCAL, LANG_PHP, LANG_PYTHON, LANG_JAVASCRIPT, LANG_RUST, LANG_GO, LANG_ADA } target_lang;
+    enum {
+        LANG_ADA,
+        LANG_C,
+        LANG_CPLUSPLUS,
+        LANG_CSHARP,
+        LANG_DART,
+        LANG_GO,
+        LANG_JAVA,
+        LANG_JAVASCRIPT,
+        LANG_PASCAL,
+        LANG_PHP,
+        LANG_PYTHON,
+        LANG_RUST
+    } target_lang;
     const char * externals_prefix;
     const char * variables_prefix;
     const char * runtime_path;
