e23d137acf8f9ceaa4df56385132113f0f6124bd
Olly Betts <olly@survex.com>
2026-04-10 07:42:46 +1200

    Resolve another generator difference

--- compiler/generator_ada.c
+++ compiler/generator_ada.c
@@ -68,8 +68,9 @@ static void write_varname(struct generator * g, struct name * p) {
 
 /* Reference to variable, e.g. when assigning to or using in an expression. */
 static void write_varref(struct generator * g, struct name * p) {
-    if (p->type < t_routine && p->local_to == NULL)
+    if (p->type < t_routine && p->local_to == NULL) {
         write_string(g, "Z.");
+    }
     write_varname(g, p);
 }
 
--- compiler/generator_c.c
+++ compiler/generator_c.c
@@ -40,8 +40,9 @@ static void write_varname(struct generator * g, struct name * p) {
 
 /* Reference to variable, e.g. when assigning to or using in an expression. */
 static void write_varref(struct generator * g, struct name * p) {
-    if (p->type < t_routine && p->local_to == NULL)
+    if (p->type < t_routine && p->local_to == NULL) {
         write_string(g, "((SN_local *)z)->");
+    }
     write_varname(g, p);
 }
 
