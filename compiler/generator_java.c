0110c138f5b2517d4d51f48e7261e498ce8296d8
Olly Betts <olly@survex.com>
2026-04-15 11:01:58 +1200

    Use write_char() to write a single character

--- compiler/generator_java.c
+++ compiler/generator_java.c
@@ -41,7 +41,7 @@ static void write_literal_string(struct generator * g, symbol * p) {
     for (int i = 0; i < SIZE(p); i++) {
         int ch = p[i];
         if (32 <= ch && ch < 127) {
-            if (ch == '\"' || ch == '\\') write_string(g, "\\");
+            if (ch == '\"' || ch == '\\') write_char(g, '\\');
             write_char(g, ch);
         } else if (ch < 128) {
             // Escape as octal.
