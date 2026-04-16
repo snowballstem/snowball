62faedfdcc943cf4fcf69b0712a958ea487cd57d
Olly Betts <olly@survex.com>
2026-04-16 21:26:28 +1200

    Handle c_backwards, c_reverse, c_test in K_needed
    These all restore the cursor on signal t, so if their subcommand
    can't fail these comamnds don't require the cursor to be restored.
    
    Improves codegen for tamil.sbl a bit.

--- compiler/generator.c
+++ compiler/generator.c
@@ -307,6 +307,13 @@ static int K_needed_(struct node * p, int call_depth) {
                 if (K_needed_(p->left, call_depth)) return true;
                 break;
 
+            case c_backwards:
+            case c_reverse:
+            case c_test:
+                if (p->possible_signals != 1) return true;
+                // Restores cursor on t and the subcommand can't fail.
+                break;
+
             default: return true;
         }
         p = p->right;
