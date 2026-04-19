snowballstem/snowball

commit a0184cd45f1f55453a29c703541884949dc10a27

Replace single-entry c_and/c_or with subnode
Previously we turned them into single-entry c_bra because it's
hard to swap in a different node at this point in the code, but
I just realised we can swap around the contents of the nodes to
achieve this.

compiler/analyser.c
@@ -2411,10 +2411,16 @@ static void visit_node(struct analyser * a, struct node * p) {
             // c_or nodes.  These can lead to unused variables in the generated
             // code and may also hinder further optimisations.
             //
-            // Ideally we'd replace these with their subnode, but that's fiddly
-            // to do as we need to update the location we got the current value
-            // of `p` from, so for now we turn them into c_bra instead.
-            p->type = c_bra;
+            // We want to replace these with their subnode.  It's fiddly to do
+            // an actual replacement as we'd need to update the location we got
+            // the current value of `p` from, so instead we swap the contents
+            // of the two nodes.
+            struct node * p_left = p->left;
+            struct node * p_right = p->right;
+            struct node tmp = *p;
+            *p = *p_left;
+            p->right = p_right;
+            *p_left = tmp;
         }
 
         p = p->right;

commit 4fbf0cd5970ec8489910966a8b4b21528541d6aa

Fix block leaks
Introduced by recent change a0184cd45f1f55453a29c703541884949dc10a27.

compiler/analyser.c
@@ -2414,12 +2414,17 @@ static void visit_node(struct analyser * a, struct node * p) {
             // We want to replace these with their subnode.  It's fiddly to do
             // an actual replacement as we'd need to update the location we got
             // the current value of `p` from, so instead we swap the contents
-            // of the two nodes.
+            // of the two nodes.  Note that we must not swap the existing ->next
+            // pointers as that could break the chain of all allocated nodes we
+            // have from analyser->nodes.
             struct node * p_left = p->left;
             struct node * p_right = p->right;
+            struct node * p_next = p->next;
             struct node tmp = *p;
+            tmp.next = p_left->next;
             *p = *p_left;
             p->right = p_right;
+            p->next = p_next;
             *p_left = tmp;
         }
 