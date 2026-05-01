161a9497801877ba9e3dc659f6d31f83362d3b7f
Olly Betts <olly@survex.com>
2026-05-01 17:31:04 +1200

    JS: Optimise among with all <-'x' actions
    We now generate a single call to slice_from() with the argument
    obtained by indexing into an array of literal strings.
    
    This is perhaps faster, albeit not by much, but it definitely
    results in smaller code, which is helpful for in browser use.
    
    See #227

--- compiler/generator_js.c
+++ compiler/generator_js.c
@@ -1255,6 +1255,20 @@ static void generate_among(struct generator * g, struct node * p) {
         /* Only one outcome ("no match" already handled). */
         generate(g, x->commands[0]);
     } else if (x->command_count > 0) {
+        if (x->same_action == c_slicefrom && x->command_count > 1) {
+            if (x->nocommand_count > 0) {
+                w(g, "~Mif (a > 0) {~N+");
+            }
+            write_comment(g, x->commands[0]);
+            g->I[0] = x->number;
+            w(g, "~Mthis.slice_from(as_~I0[a - 1]);~N");
+            if (x->nocommand_count > 0) {
+                write_block_end(g);
+            }
+            g->unreachable = false;
+            return;
+        }
+
         w(g, "~Mswitch (a) {~N~+");
         for (int i = 1; i <= x->command_count; i++) {
             g->I[0] = i;
@@ -1425,6 +1439,16 @@ static void generate_among_table(struct generator * g, struct among * x) {
         w(g, "]");
     }
     w(g, "~N~-~M];~N~N");
+
+    if (x->same_action == c_slicefrom && x->command_count > 1) {
+        g->I[0] = x->number;
+        w(g, "~Mconst /** Array<string> */ as_~I0 = [");
+        for (int i = 1; i <= x->command_count; i++) {
+            if (i > 1) w(g, ", ");
+            write_literal_string(g, x->commands[i - 1]->left->literalstring);
+        }
+        w(g, "];~N~N");
+    }
 }
 
 static void generate_amongs(struct generator * g) {
